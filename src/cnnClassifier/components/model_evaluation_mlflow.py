1b7cfdb5fd7e1153daa47a1d0f4d7601687d2c8d
ABK <bhargava07491@gmail.com>
2023-12-04 16:36:59 +0530

    prediction added

--- src/cnnClassifier/components/model_evaluation_mlflow.py
+++ src/cnnClassifier/components/model_evaluation_mlflow.py
@@ -6,6 +6,9 @@ from urllib.parse import urlparse
 from pathlib import Path
 from cnnClassifier.entity.config_entity import EvaluationConfig
 from cnnClassifier.utils.common import read_yaml, create_directories, save_json
+
+
+
 class Evaluation:
     def __init__(self, config: EvaluationConfig):
         self.config = config
